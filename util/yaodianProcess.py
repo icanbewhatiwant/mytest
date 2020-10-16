dd702fea8c907e05f3c4fe88a1d6cd3f6502ac48
jenna.qu <qujiahappy@163.com>
2020-10-16 17:57:30 +0800

    药典抽取修改

--- util/yaodianProcess.py
+++ util/yaodianProcess.py
@@ -2,9 +2,9 @@ from docx import Document
 import json
 import re
 
-
 def data2Excel(drug_dict,drug_list,pra_len):
     pra_lens = pra_len-1
+    dk = 0 #记录@出现次数
     for i,par in enumerate(paragraphs): #获得段落对象列表
         par_str = par.text
         # if i <=50:
@@ -12,37 +12,58 @@ def data2Excel(drug_dict,drug_list,pra_len):
             continue
 
         #提取药品中、英文名称
-        en_match = enpattern.search(par_str)
-        if en_match:
+        drug_match = label_drug.search(par_str)
+        if drug_match:
+            dk+=1
             #匹配到药品英文名称时，字典内容作为一个整体存入list
-            if drug_dict:
+            if drug_dict and dk==1:
                 drug_list.append(drug_dict)
                 drug_dict={}
 
-            en_name = par_str
-            k = i + 1
-            while True:#处理英文名称换行
-                if k <= pra_lens:
-                    next_en_text = paragraphs[k].text
-                    next_en_match = enpattern.search(next_en_text)
-                    if next_en_match:
-                        en_name += " /"+ next_en_text
-                        k +=1
+            drug_name = par_str
+            yb_info = ""
+            en_name = ""
+            if dk ==1:
+                k = i + 1
+                while True:#
+                    if k <= pra_lens:
+                        next_text = paragraphs[k].text
+                        next_match = label_drug.search(next_text)
+                        if next_match:
+                            dk+=1
+                            if dk==2:
+                                yb_info = next_text
+                                break
+                        else:#本句没匹配到标题或医保，判断下一句是否匹配到标签符号，匹配到则本句是英文名称
+                            while True:
+                                m = k+1
+                                if m <=pra_lens:
+                                    nex_nex_text = paragraphs[m].text
+                                    if nex_nex_text =="":
+                                      continue
+                                    label_match = label_patr.search(nex_nex_text)
+                                    if label_match :
+                                        en_name = next_text
+                                    break
                     else:
                         break
+
+            dk = 0
             drug_dict["enName"] = en_name
-            drug_dict["drugName"] = paragraphs[i-1].text
-            # print("drug_name", drug_name)
-            # print("en_name:", en_name)
+            drug_dict["drugName"] = drug_name
+            drug_dict["yb_info"] = yb_info
 
         #提取各种标签及其对应内容
-        match = label_patr.search(par_str)
-        if match:
-            label_str = match.group()
+        label_match = label_patr.search(par_str)
+
+        if label_match:
+            label_str = label_match.group()
             #因为括号中可能有.或者其他符号，找到括号中的全部中文，拼接为label
             label_cmatch = label_con.findall(label_str)
+            label = ""
             if label_cmatch:
                 label = ''.join(label_cmatch)
+
             label_len = len(label_str)
             content = par_str[label_len:]
 
@@ -55,15 +76,12 @@ def data2Excel(drug_dict,drug_list,pra_len):
                     if next_text =="":
                         j+=1
                         continue
-                    # 匹配到第几节、第几章
-                    if chapter_patr.search(next_text):
+
+                    # 下一段匹配到药品名称
+                    nex_label_match = label_drug.search(next_text)
+                    if nex_label_match:
                         break
-                    # 下下段匹配到药品英文名称，则下段是药品名称
-                    if j+1 <=pra_lens:
-                        nex_nex_text = paragraphs[j+1].text
-                        nexen_match = enpattern.search(nex_nex_text)
-                        if nexen_match:
-                            break
+
                     # 下一段匹配到label
                     if label_patr.search(next_text):
                         break
@@ -80,11 +98,10 @@ def data2Excel(drug_dict,drug_list,pra_len):
         # else:
         #     break
     if drug_list:
-        with open("C:/产品文档/转换器测试数据/1401-1539补充数据.json","w",encoding='utf-8') as fp:
+        with open("C:/产品文档/转换器测试数据/1-100.json","w",encoding='utf-8') as fp:
             fp.write(json.dumps(drug_list, indent=4,ensure_ascii=False))#unicode串转中文传入
 
 #遍历文件得到label名称，去重作为列名
-
 # 得到：['【药理】', '【不良反应】', '【禁忌证】', '【注意事项】', '【用法与用量】', '【儿科用法与用量】', '【制剂与规格】', '【适应证】', '【儿科注意事项】', '【药物相互作用】', '【给药说明】']
 def getLabellist():
     label_list=[]
@@ -122,19 +139,15 @@ if __name__ == "__main__":
     # 源数据是pdf转word，数据不规范
     # 标签，前面括号格式有中英文全半角的各种格式组合
     label_drug = re.compile("[@|＠]")
+    #标签对应内容提取
     label_patr = re.compile("^(.{,2}【|\[|\[:|［:|［|.【)[^】|\]|］]+(】|\]|］)")  # 中间不能有】，实现非贪婪
     label_con = re.compile("[\u4e00-\u9fa5]+")
-    # 匹配药品英文名称
-    enpattern = re.compile(u'^[a-zA-Z\s/-]+$')#包含"/"
-
-    # 匹配章、节
-    chapter_patr = re.compile('第.+章|第.+节')
 
     drug_dict = {}
     drug_list = []
 
-    getLabellist()
-    # data2Excel(drug_dict,drug_list,pra_len)
+    # getLabellist()
+    data2Excel(drug_dict,drug_list,pra_len)
 
 
 
