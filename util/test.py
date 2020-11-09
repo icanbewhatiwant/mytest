811ab91b613007e69c7c0a735177d303784e0108
jenna.qu <qujiahappy@163.com>
2020-11-09 17:55:32 +0800

    update

--- util/test.py
+++ util/test.py
@@ -17,10 +17,7 @@ function_str = "(镇静|催眠|镇静.催眠|抗恐惧|抗?癫痫|抗?失眠|一
                "|脑梗死急性期|脑外伤及脑手术后的意识障碍|良性记忆障碍|阿尔茨海默病.血管性痴呆|阿尔茨海默病和?.{0,3}血管性痴呆|阿尔茨海默病|重症肌无力、肌营养不良症、多发性周围神经病|帕金森病、多发性硬化症及痉挛状态" \
                "|获得性振动性眼球震颤|神经性膀胱功能障碍|假性近视|确?诊?重症肌无力的?确?证?|治疗重症肌无力|重症肌无力|术后腹胀气或尿潴|对抗非去极化型肌松药的肌松作用|麻醉诱导|全麻.?诱导|全身麻醉|全麻诱导|平衡麻醉|全凭静脉麻醉|全麻维持量|局部麻醉或椎管内麻醉辅助用药" \
                "|眼科用|耳鼻喉科用|硬膜外麻醉用|髄管阻滞|硬膜外阻滞?|区域阻滞|神经传导阻滞|黏膜表面麻醉|蛛网膜下隙阻滞|外周神经阻滞|交感神经节阻滞|神经干（丛）阻滞|胃镜检査|尿道扩张术或膀胱镜检査|臂丛神经阻滞|紙管阻滞|硬脊膜外阻滞|局部浸润" \
-               "|硬膜外腔阻滞|神经阻滞|手术后镇痛|分娩镇痛|手?术中维持肌松|维持肌松|电休克|气管插管|维持肌肉松弛|半去极化肌松药的拮抗|青光眼|呕吐|精神分裂症|肾功能不全|肝功能不全|遗尿症|抗高胆红素血症|缓释制剂)"
-
-age_str = "(肝、肾功能损害者|老年人|成人|老年和体弱或肝功能不全患者|特殊人群：严重肝损患者|老年或虚弱的患者|老年或虚弱者|年老体弱者|老年、重病和肝功能受损患者|重症患者|肝、肾疾病患者" \
-          "|老年、女性、非吸烟、有低血压倾向、严重肾功能 损害或中度肝功能损害患者|新生儿)"
+               "|硬膜外腔阻滞|神经阻滞|手术后镇痛|分娩镇痛|手?术中维持肌松|维持肌松|电休克|气管插管|维持肌肉松弛|半去极化肌松药的拮抗|青光眼|呕吐|精神分裂症|肾功能不全|肝功能不全|遗尿症|抗高胆红素血症|缓释制剂|治疗心律失常|外科硬膜外腔阻滞麻醉|拮抗东芨着碱中毒|用于青光眼|静脉全麻)"
 
 bracket_str = "测试（1） 口服成人抗焦虑，一次0.5〜 1 mg,一日2〜3次。镇静催眠。睡前服2〜4 mg。年老体弱 者应减量。12岁以下小儿安全性与剂量尚未确定。&nsp（2）\t肌内注射抗焦虑、镇静催眠,一次按体重 0. 05 mg/kg,总量不超过4 mg。&nsp（3）\t静脉注射 用于癌症化疗止吐，在化疗前30分 钟注射2〜4 mg,与奋乃静合用效果更佳,必要时重复使 用给药；癫痫持续状态,按体重0. 05 mg/kg,一次不超过 4 mg,如10〜15分钟后发作仍继续或再发。可重复注射 0. 05 mg/kg,如再经10〜15分钟仍无效。需采用其他措 施，12小时内用量一般不超过8 mg。"
 #效果：[test(1)……,test(2)……，……]
@@ -35,28 +32,12 @@ def get_bracket_str(str):
             result.append(str0)
         result1 = ["".join(i) for i in zip(result1[1::2], result1[2::2])]
         result.extend(result1)
-
-    # bracket_patr = re.compile(r"([（(]\d[）)])+?")
-    # bracket_f = re.finditer(bracket_patr,str)
-    # indexes = [i.start() for i in bracket_f]
-    # start = 0
-    # result1 = []
-    # for i,indx in enumerate(indexes):
-    #     if indx==0:
-    #         continue
-    #     sbstr = str[start:indx]
-    #     result1.append(sbstr)
-    #     if i == len(indexes)-1:
-    #         sbstr1 = str[indx:]
-    #         result1.append(sbstr1)
-    #     start = indx
     take_str = result[0]
     take_patr_b = re.compile("^([（(]\d[）)])+")
     # (1)序号前有字符串（服药方式）,拼接
     if not take_patr_b.search(take_str):
         result = ["".join(i) for i in zip([take_str]*(len(result)-1),result[1::1])]
     return result
-
 # result = get_bracket_str(bracket_str)
 # print("get_bracket_str",result)
 
@@ -80,35 +61,29 @@ def get_circle_str(str):
         result = ["".join(i) for i in zip([take_str] * (len(result) - 1), result[1::1])]
 
     return result
-
 # result1 = get_circle_str("（2）肌内或静脉注射成人①催眠，一次100〜200 mg；镇静，一次30〜50 mg,一日2〜3次；②抗惊厥（常 用于治疗癫痫持续状态），缓慢静脉注射300〜500 mg。 成人极量一次0. 25 g,—日0. 5g。")
 # print("circle_cut:",result1)
 
 
-#句内按指定分号切分
-# sub_str = "②镇静催眠，0. 4〜0.8mg,睡前服。老年和 体弱患者开始用小量，一次0. 2 mg, 一日3次，逐渐递增 至最大耐受量。"
-# 结果效果：result = ['②镇静催眠，0.4〜0.8mg,睡前服。', '②镇静催眠，老年和体弱患者开始用小量，一次0.2mg,一日3次，逐渐递增至最大耐受量。']
+#1-200页句内按指定字符切分，仅拼接句前部分句子以及服用方式
 semicolon_str = "([；;]用于|[；;]或|[；;]用作|[；;]硬膜外麻醉|[；;]臂丛神经阻滞麻醉|[；;]各种神经阻滞或硬膜外麻醉|[；;]镇静|[；;]催眠|[；;]抗惊厥|[；;]皮下或肌内注射)+"
 #100-200页内分号切分个例
-semicolon_12_zd = "([;；]快速气管插管|[;；]癫痫持续状态|[;；]麻醉前用药|[;；]术后应用|[;；]拮抗东芨着碱中毒|[;；]癫痫持续状态|[;；]牙科|[;；]肋间神经|[;；]宫颈旁浸润|[;；]椎旁脊神经阻滞" \
-                  "|[;；]阴部神经|药物诱发的锥体外系反应|药物诿发的锥体外系反应|注入蛛网膜下隙|硬膜外PCA|，背景输注量|重度疼痛|如不能控制|用作胶原酶合成抑制剂时|一过性失眠|或按体表面积|用量视患者的耐受情况)"
-#100-200页内无分号切分个例
-semicolon_12_str = "用作胶原酶合成抑制剂时"
-semiconlon_patr = re.compile(semicolon_str)
+semicolon_12_zd = "([；;]用于|[；;]或|[；;]用作|[；;]硬膜外麻醉|[；;]臂丛神经阻滞麻醉|[；;]各种神经阻滞或硬膜外麻醉|[；;]镇静|[；;]催眠|[；;]抗惊厥|[；;]皮下或肌内注射|[;；]快速气管插管|[;；]癫痫持续状态|[;；]麻醉前用药|[;；]术后应用|[;；]拮抗东芨着碱中毒|[;；]癫痫持续状态|[;；]牙科|[;；]肋间神经|[;；]宫颈旁浸润|[;；]椎旁脊神经阻滞" \
+                  "|[;；]阴部神经|药物诱发的锥体外系反应|药物诿发的锥体外系反应|注入蛛网膜下隙|硬膜外PCA|，背景输注量|重度疼痛|如不能控制|用作胶原酶合成抑制剂时|一过性失眠" \
+                  "|或按体表面积|用量视患者的耐受情况|全麻维持，成人可釆用连续静脉滴注|辅助椎管内麻醉|使用靶控输注系统给药|尿道扩张术|治疗心律失常|；0.5%溶液|；0.75%溶液|外科硬膜外腔阻滞麻醉|用于神经阻滞麻醉" \
+                  "|用于硬膜外腔阻滞麻醉|用于硬膜外阻滞|,吸入麻醉|用作静脉全麻|肛门给药栓剂|缓释片用法|对精神分裂患者|高龄患者|部分患者|也有患者)+"
+semiconlon_patr = re.compile(semicolon_12_zd)
 take_patr = re.compile(admin_route_str)
 function_patr = re.compile(function_str)
 
-#按分号切分句子
-# import re
-def get_concat_str(search_string,param_list=None):
+#将句首中的给药方式作为拼接字符串返回
+def get_concat_str(search_string):
     concat_string = ""
     admin_search = take_patr.search(search_string)
-    if param_list:
-        begin_str = ''.join(param_list)
-        concat_string += begin_str
     if admin_search:
-        admin_route = admin_search.groups() #返回re.compile()中字符串中所有括号匹配部分
-        concat_string +=  ','.join(admin_route) + '。'
+        # admin_route = admin_search.groups() #返回re.compile()中字符串中所有括号匹配部分
+        admin_route = take_patr.findall(search_string)#以列表形式返回全部能匹配的子串
+        concat_string +=  ','.join(admin_route)
     return concat_string
 
 def get_semi_cut(str):
@@ -117,7 +92,7 @@ def get_semi_cut(str):
     semi_result = []
     result = []
     if semiconlon_patr.search(str):
-        result = re.split(semicolon_str,str)
+        result = re.split(semicolon_12_zd,str)
     else:
         semi_result.append(str)
     len_semi = len(result)
@@ -125,71 +100,88 @@ def get_semi_cut(str):
         semi_result.append(result[0])
         result12last = [''.join(i) for i in zip(result[1::2],result[2::2])]
         semi_result.extend(result12last)
-    #拼接
-
+    #拼接给药方式
     take_patr_b = re.compile("^([（(]\d[）)])+")
     take_patr_cir = re.compile("([①②③④⑤⑥⑦⑧⑨⑩])+")
     b_match = take_patr_b.search(str)
     cir_match = take_patr_cir.search(str)
-    concat_string = ""
 
     if semi_result:
         if len_semi >1:
             concat_str = semi_result[0]
-            #有（1）标号
-            if b_match:
-                # 有（1）标号，有①标号，直接拼接(1)和①标号之间的内容，①标号中判断是否有服用方式和作用，服用方式拼接，作用在断句中有则不需拼接，没有要拼接
-                if cir_match:
-                    concat_strb = str[:cir_match.start()]#(1)……
-                    concat_strcir = str[cir_match.start():]#①……
-                    param_str = [concat_strb,cir_match.group()]
-                    concat_string = get_concat_str(concat_strcir,param_str)
-                # 有（1）标号，没有①标号，判断（1）句是否有服用方式和作用，服用方式需拼接，作用在断句中有则不需拼接，没有要拼接
-                else:
-                    param_str = [b_match.group()]
-                    concat_string = get_concat_str(concat_str, param_str)
-            #无（1）标号
-            else:
-                # 没有(1)标号，有①标号，前面有文字的直接拼接，①标号后的文字中判断是否有服用方式和作用，服用方式拼接，作用在断句中有则不需拼接，没有要拼接
-                if cir_match:
-                    before_cir_str = concat_str[:cir_match.start()]
-                    param_str = [before_cir_str,cir_match.group()]
-                    after_cir_str = concat_str[cir_match.start():]
-                    concat_string = get_concat_str(after_cir_str, param_str)
-                # 没有(1)标号，没有①标号，判断本句有没有服用方式和作用，没有的话判断前面第一句（下标0）是否有服用方式和作用，本句有则拼接，没有不管
-                else:
-                    param_str = []
-                    concat_string = get_concat_str(concat_str, param_str)
-
             for i,con in enumerate(semi_result):
+                concat_string = ""
                 if i == 0:
                     continue
-                function_search = function_patr.search(con)
+                #判断句子是否有服药方式，有则不需拼接需要方式，没有要拼接
+                take_search = take_patr.search(con)
+                # 有（1）标号
+                if b_match:
+                    # 有（1）标号，有①标号，直接拼接(1)和①标号之间的内容，①标号后开始断句处判断是否有作用，没有则拼接
+                    if cir_match:
+                        concat_strb = str[:cir_match.start()]  # (1)……
+                        concat_strcir = str[cir_match.start():]  # ①……
+                        param_str = [concat_strb, cir_match.group()]
+                        if param_str:
+                            begin_str = ''.join(param_str)
+                            concat_string += begin_str
+                        if not take_search:
+                            concat_string += get_concat_str(concat_strcir)
+                    # 有（1）标号，没有①标号，判断断句是否有服用方式，没有则拼接包含标号（1）的首句中的服用方式
+                    else:
+                        param_str = [b_match.group()]
+                        if param_str:
+                            begin_str = ''.join(param_str)
+                            concat_string += begin_str
+                        if not take_search:
+                            concat_string += get_concat_str(concat_str)
+                    # 无（1）标号
+                else:
+                    # 没有(1)标号，有①标号，前面有文字的直接拼接，①标号后断句判断是否有服用方式，没有拼接句首中服用方式
+                    if cir_match:
+                        before_cir_str = concat_str[:cir_match.start()]
+                        param_str = [before_cir_str, cir_match.group()]
+                        after_cir_str = concat_str[cir_match.start():]
+                        if param_str:
+                            begin_str = ''.join(param_str)
+                            concat_string += begin_str
+                        if not take_search:
+                            concat_string += get_concat_str(after_cir_str)
+                    # 没有(1)标号，没有①标号，判断本句有没有服用方式，没有的话判断前面第一句（下标0）是否有服用方式，本句有则不拼接，没有拼接
+                    else:
+                        param_str = []
+                        if not take_search:
+                            concat_string = get_concat_str(concat_str)
+                # 这一段决定不用了，因为拼接作用有出错的可能性，而作用并不需要体现在断句正确性中，所以这里不拼接作用了
                 #本句没有作用，则拼接第一句的作用
-                if not function_search:
-                    begin_function_search = function_patr.search(concat_str)
-                    function_list = begin_function_search.groups()
-                    concat_string += ','.join(function_list)
+                # function_search = function_patr.search(con)
+                # if not function_search:
+                #     begin_function_search = function_patr.search(concat_str)
+                #     if begin_function_search:
+                #         function_list = begin_function_search.groups()
+                #         concat_string += ','.join(function_list)
                 semi_result[i] = concat_string + con
-
     # 断句部分还要补充
     return semi_result
 semi_str1 = "（2）肌内或静脉注射成人①催眠，一次100〜200 mg；镇静，一次30〜50 mg,一日2〜3次；"
 # semi_cut: ['（2）肌内或静脉注射成人①催眠，一次100〜200mg', '（2）肌内或静脉注射成人①；镇静，一次30〜50mg,一日2〜3次；']
 semi_str2="（1） 口服 成人 抗癫痫一般一次 0.03g，一日3次；或0. 09 g睡前顿服。极量一次0. 25 g。一日 0. 5g。"
-# semi_cut: ['（1）口服成人抗癫痫一般一次0.03g，一日3次', '（1）口服成人。抗癫痫；或0.09g睡前顿服。极量一次0.25g。一日0.5g。']
+# semi_cut: ['（1）口服成人抗癫痫一般一次0.03g，一日3次', '（1）口服成人。或0.09g睡前顿服。极量一次0.25g。一日0.5g。']
 semi_str3 = "肌内或静脉注射成人①催眠，一次100〜200 mg；镇静，一次30〜50 mg,一日2〜3次；"
 # semi_cut: ['肌内或静脉注射成人①催眠，一次100〜200mg', '肌内或静脉注射成人①；镇静，一次30〜50mg,一日2〜3次；']
 semi_str4 = "肌内或静脉注射成人催眠，一次100〜200 mg；镇静，一次30〜50 mg,一日2〜3次；"
 # semi_cut: ['肌内或静脉注射成人催眠，一次100〜200mg', '肌内或静脉注射。；镇静，一次30〜50mg,一日2〜3次；']
+test_zd="可口服、皮下注射、肌内注射、静脉注 射及肛门内给药。一次50〜100 mg,一日2〜3次。一日不 超过400 mg,老年患者一日不超过300 mg。重度疼痛可一 次100 mg开始。肛门给药栓剂一次100 mg,一日1〜2次。"
+# semi_cut: ['可口服、皮下注射、肌内注射、静脉注射及肛门内给药。一次50〜100mg,一日2〜3次。一日不超过400mg,老年患者一日不超过300mg。', '口服,皮下注射,肌内注射,静脉注射,肛门内给药。重度疼痛可一次100mg开始。', '肛门给药栓剂一次100mg,一日1〜2次。']
 
-print("semi_cut:",get_semi_cut(semi_str3))
+print("semi_cut:",get_semi_cut(semi_str2))
 
-
-
-test_sub_list =['口服 成人 ①抗焦虑，开始一次 0.4〜1.2mg,一日2次，用量按需递增。最大限量一日 可达4 mg。', '口服 成人 ②镇静催眠，0. 4〜0.8mg,睡前服。老年和 体弱患者开始用小量，一次0. 2 mg, 一日3次，逐渐递增 至最大耐受量。', '口服 成人 ③抗恐惧，一次0.4 mg,一日3次，需要 时逐渐增加剂量，一日最大量可达10 mg。']
 #按年龄+单次服用剂量判断切分
+age_str = "(成人|肝、肾功能损害者|老年人|老年和体弱或肝功能不全患者|老年人?[或及、]?体弱患者|老年体弱者|年老体弱者|特殊人群：严重肝损患者|老年或虚弱的患者|老年人?或虚弱患?者|老年、重病和肝功能受损患者" \
+          "|老年患者|重症患者|肝、肾疾病患者|老年、女性、非吸烟、有低血压倾向、严重肾功能 损害或中度肝功能损害患者|新生儿|幼儿和儿童|幼儿|儿童|儿童少年" \
+          "|<?\d*岁|\d*[-|〜|~|~]?\d*岁小儿|\d岁以上患儿|\d*[-|〜|~|~]\d*岁|\d*岁以上|\d*岁以下|>\d*岁||d*岁或以上者|小儿)"
 age_patr = re.compile("[，。,.;；]+?(<1岁|幼儿|小儿|\d+〜\d+岁小儿|\d+岁以上患儿|成人|老年.?体弱.?者|年老.?体弱.?者|老年人|儿童)+")
+age_zd="……的患者"
 circle_sub_patr = re.compile("([①②③④⑤⑥⑦⑧⑨⑩]+)[^，。,]+[，。,]+?")#匹配序列标号后第一个句子
 def get_age_cut(str):
     str = str.replace("&nsp", "").replace("\t", "").replace(" ", "")
