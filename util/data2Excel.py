24211b588f47f7c72159ded022625a1827a6dd46
jenna.qu <qujiahappy@163.com>
2020-11-20 17:44:13 +0800

    update

--- util/data2Excel.py
+++ util/data2Excel.py
@@ -26,17 +26,17 @@ def pre_process():
     data_df["儿科用法与用量"] = names
     data_df["制剂与规格"] = names
     data_df["儿科注意事项"] = names
-    data_df["药物相互作用"]
+    data_df["药物相互作用"] = names
 
 
     #创建excel文件
-    writer = pd.ExcelWriter('C:/产品文档/转换器测试数据/test.xlsx')
+    writer = pd.ExcelWriter('C:/产品文档/转换器测试数据/shenme.xlsx')
     # result2.to_excel(writer, float_format='%.5f')
     data_df.to_excel(writer,index = False,encoding='UTF-8')#不保存索引
     writer.save()
     writer.close()
 
-pre_process()
+# pre_process()
 
 #针对每个备份文件都要进行：
 #遍历去重得到文件中的label名称作为列名称
@@ -45,7 +45,10 @@ pre_process()
 #删除每个备份文件中不符合格式处理的数据
 #修改label中不对的数据
 #数据存储到json文件中，最后得到keys进行最终的列名list，最后再存入excel数据
-
-
+if __name__=="__main__":
+    dict = {"年龄低值":"","年龄高值":""}
+    df = pd.DataFrame(columns=['年龄低值','年龄高值'])
+    df.iloc[0,0] = "1111"
+    print(df)
 
 
