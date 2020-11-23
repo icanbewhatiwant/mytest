3e7a51e756bca9715278e981b04bf554c074723a
jenna.qu <qujiahappy@163.com>
2020-11-23 15:01:40 +0800

    update

--- util/data2Excel.py
+++ util/data2Excel.py
@@ -109,33 +109,9 @@ if __name__=="__main__":
     limit_1day = []
     recommand_days_low = []
     recommand_days_high = []
+    cut_senteces = []
 
-    data_df["药品名称"] = drug_names
-    data_df["用药用量"] = dose_info
-    data_df["儿科用药用量"] = erke_dose_info
-    data_df["审查结果"] = sentences
-
-    # 字段
-    data_df["年龄低值"] = age_low
-    data_df["年龄高值"] = age_high
-    data_df["年龄单位（岁；月；天）"] = age_unit
-    data_df["体重低值"] = weight_low
-    data_df["体重高值"] = weight_high
-    data_df["给药途径"] = admin_route
-    data_df["单次推荐剂量低值"] = sdose_low
-    data_df["单次推荐剂量高值"] = sdose_high
-    data_df["单次剂量极值"] = limit_1time
-    data_df["单日推荐剂量低值"] = sday_dose_low
-    data_df["单日推荐剂量高值"] = sday_dose_high
-    data_df["单日剂量极值"] = limit_1day
-    data_df["剂量单位"] = single_dose_unit
-    data_df["推荐给药频次低值"] = dose_time_low
-    data_df["推荐给药频次低值描述"] = dose_time_low_des
-    data_df["推荐给药频次高值"] = dose_time_high
-    data_df["推荐给药频次高值描述"] = dose_time_high_des
-    data_df["用药推荐天数低值"] = recommand_days_low
-    data_df["用药推荐天数高值"] = recommand_days_high
-
+    #读取切分结果，excel保存
     def data_process(filepath):
         tmp = []
         json_str = ""
@@ -144,20 +120,15 @@ if __name__=="__main__":
             if check_json_format(json_str):
                 tmp.append(json.loads(json_str))
                 json_str = ""
-
-
-
-
         if tmp:
             for drug_info in tmp:
                 s_result_list = []
                 e_s_result_list =[]
-                drugName = drug_info.get("drugName", "")
+                drugName = drug_info.get("drugName", "").replace("@", "")
                 if drugName !="":
 
                     drug_names.append(drugName)
-                    dose_info.append(drug_info.get("用法与用量",""))
-                    erke_dose_info.append(drug_info.get("儿科用法与用量",""))
+                    dose_info.append(drug_info.get("用法与用量","").replace("&nsp", "").replace("\t", "").replace(" ", ""))
 
                     #用药用量分句和结果list[{"1":……,"2":……},{},……] 1：分句 2：对应字段
                     s_result_list = drug_info.get("s_result",[])
@@ -176,43 +147,108 @@ if __name__=="__main__":
 
                     if sentence_length >1:
                         drug_names.extend([""]*(sentence_length - 1))
-                    if e_s_result_len >0:
-                        s_result_list.extend([{}]*e_s_result_len)
+
+                    #拼接用法用量下的空字符串 用法……
+                    if sentence_length >1:
+                        dose_info.extend([""]*(sentence_length-1))
+                    #拼接儿科用法用量的空字符串 ……儿科……
                     if s_result_len > 0:
+                        erke_dose_info.extend([""]*s_result_len)
+                    if e_s_result_len >0:
+                        erke_dose_info.append(drug_info.get("儿科用法与用量", "").replace("&nsp", "").replace("\t", "").replace(" ", ""))
+                        erke_dose_info.extend([""]*(e_s_result_len-1))
 
 
+                    #审查结果：断句切分
+                    if s_result_list:
+                        for sen_ziduan in s_result_list:
+                            sent_string = sen_ziduan.get("1","")
+                            if sent_string != "":
+                                cut_senteces.append(sent_string)
+                                sen_result = sen_ziduan.get("2",{})
+                                # if sen_result:
+                                age_low.append(sen_result.get("age_low",""))
+                                age_high.append(sen_result.get("age_high",""))
+                                age_unit.append(sen_result.get("age_unit",""))
+                                weight_low.append(sen_result.get("weight_low",""))
+                                weight_high.append(sen_result.get("weight_high",""))
+                                admin_route.append(sen_result.get("admin_route",""))
+                                sdose_low.append(sen_result.get("sdose_low",""))
+                                sdose_high.append(sen_result.get("sdose_high",""))
+                                dose_time_low.append(sen_result.get("dose_time_low",""))
+                                dose_time_high.append(sen_result.get("dose_time_high",""))
+                                dose_time_low_des.append(sen_result.get("dose_time_low_des",""))
+                                dose_time_high_des.append(sen_result.get("dose_time_high_des",""))
+                                sday_dose_low.append(sen_result.get("sday_dose_low",""))
+                                sday_dose_high.append(sen_result.get("sday_dose_high",""))
+                                single_dose_unit.append(sen_result.get("single_dose_unit",""))
+                                limit_1time.append(sen_result.get("limit_1time",""))
+                                limit_1day.append(sen_result.get("limit_1day",""))
+                                recommand_days_low.append(sen_result.get("recommand_days_low",""))
+                                recommand_days_high.append(sen_result.get("recommand_days_high",""))
+                    if e_s_result_list:
+                        for erke_sen_ziduan in e_s_result_list:
+                            #儿科数据解析
+                            ek_sent_string = erke_sen_ziduan.get("1", "")
+                            if ek_sent_string != "":
+                                cut_senteces.append(ek_sent_string)
+                                sen_result = erke_sen_ziduan.get("2", {})
+                                # if sen_result:
+                                age_low.append(sen_result.get("age_low", ""))
+                                age_high.append(sen_result.get("age_high", ""))
+                                age_unit.append(sen_result.get("age_unit", ""))
+                                weight_low.append(sen_result.get("weight_low", ""))
+                                weight_high.append(sen_result.get("weight_high", ""))
+                                admin_route.append(sen_result.get("admin_route", ""))
+                                sdose_low.append(sen_result.get("sdose_low", ""))
+                                sdose_high.append(sen_result.get("sdose_high", ""))
+                                dose_time_low.append(sen_result.get("dose_time_low", ""))
+                                dose_time_high.append(sen_result.get("dose_time_high", ""))
+                                dose_time_low_des.append(sen_result.get("dose_time_low_des", ""))
+                                dose_time_high_des.append(sen_result.get("dose_time_high_des", ""))
+                                sday_dose_low.append(sen_result.get("sday_dose_low", ""))
+                                sday_dose_high.append(sen_result.get("sday_dose_high", ""))
+                                single_dose_unit.append(sen_result.get("single_dose_unit", ""))
+                                limit_1time.append(sen_result.get("limit_1time", ""))
+                                limit_1day.append(sen_result.get("limit_1day", ""))
+                                recommand_days_low.append(sen_result.get("recommand_days_low", ""))
+                                recommand_days_high.append(sen_result.get("recommand_days_high", ""))
 
-                    erke_take_way = drug_info.get("erke_sentence_cut", "")
-                    erke_take_result_list = []
-                    if take_way != "":
-                        for take_string in take_way:
-                            take_result = {}
-                            take_way_dict = get_gruguse_result(take_string)
-                            take_result["1"] = take_string
-                            take_result["2"] = take_way_dict
-                            take_result_list.append(take_result)
-                        del drug_info["sentence_cut"]
-                        if take_result_list:
-                            drug_info["s_result"] = take_result_list
+        data_df["药品名称"] = drug_names
+        data_df["用药用量"] = dose_info
+        data_df["儿科用药用量"] = erke_dose_info
+        data_df["审查结果"] = cut_senteces
 
-                    if erke_take_way != "":
-                        for erke_take_string in erke_take_way:
-                            erke_take_result = {}
-                            erke_take_way_dict = get_gruguse_result(erke_take_string)
-                            erke_take_result["1"] = erke_take_string
-                            erke_take_result["2"] = erke_take_way_dict
-                            erke_take_result_list.append(erke_take_result)
-                        del drug_info["erke_sentence_cut"]
-                        if erke_take_result_list:
-                            drug_info["e_s_result"] = erke_take_result_list
+        # 字段
+        data_df["年龄低值"] = age_low
+        data_df["年龄高值"] = age_high
+        data_df["年龄单位（岁；月；天）"] = age_unit
+        data_df["体重低值"] = weight_low
+        data_df["体重高值"] = weight_high
+        data_df["给药途径"] = admin_route
+        data_df["单次推荐剂量低值"] = sdose_low
+        data_df["单次推荐剂量高值"] = sdose_high
+        data_df["单次剂量极值"] = limit_1time
+        data_df["单日推荐剂量低值"] = sday_dose_low
+        data_df["单日推荐剂量高值"] = sday_dose_high
+        data_df["单日剂量极值"] = limit_1day
+        data_df["剂量单位"] = single_dose_unit
+        data_df["推荐给药频次低值"] = dose_time_low
+        data_df["推荐给药频次低值描述"] = dose_time_low_des
+        data_df["推荐给药频次高值"] = dose_time_high
+        data_df["推荐给药频次高值描述"] = dose_time_high_des
+        data_df["用药推荐天数低值"] = recommand_days_low
+        data_df["用药推荐天数高值"] = recommand_days_high
 
-            with open("C:/产品文档/转换器测试数据/1-200_20201120_ziduan.json", "w", encoding='utf-8') as fp:
-                for drug in tmp:
-                    fp.write(json.dumps(drug, indent=4, ensure_ascii=False))
-                    fp.write('\n')
+        # 创建excel文件
+        writer = pd.ExcelWriter('C:/产品文档/转换器测试数据/100-200.xlsx')
+        # result2.to_excel(writer, float_format='%.5f')
+        data_df.to_excel(writer, index=False, encoding='UTF-8')  # 不保存索引
+        writer.save()
+        writer.close()
 
 
-    filepath = "C:/产品文档/转换器测试数据/1-200_20201120_cutsentence.json"
-    pre_process()
+    filepath = "C:/产品文档/转换器测试数据/1-200_20201120_ziduan.json"
+    data_process(filepath)
 
 
